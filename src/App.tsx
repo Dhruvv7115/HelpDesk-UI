4a5bcbea3327553a3f5639f3b305ff25bd360f39
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-02-08 00:40:41 +0530

    feat: Add react-router-dom for routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/landing";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<main className="w-full min-h-screen flex items-center justify-center">
-			
-
-		</main>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={<Landing />}
+				></Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
